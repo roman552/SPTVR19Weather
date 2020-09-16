4a3e18effdd72ba849b1b4268ec6fd4fbf04d311
roman ivanov <roman.ivanov@ivkhk.ee>
2020-09-16 13:55:55 +0300

    заготовка проекта

---
+++ src/sptvr19weather/SPTVR19Weather.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptvr19weather;
+
+/**
+ *
+ * @author user
+ */
+public class SPTVR19Weather {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+    
+}
